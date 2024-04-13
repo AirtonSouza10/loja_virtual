3a26ff2d436d52662acd37327a72a742108b03c1
AirtonSouza10 <airtonsouza.ti@gmail.com>
2024-04-13 10:24:24 -0300

    Adição de Categoria e Marcado produto

---
+++ src/main/java/com/loja_virtual/model/CategoriaProduto.java
@@ -0,0 +1,33 @@
+package com.loja_virtual.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Entity
+@Table(name="categoria_produto")
+@AllArgsConstructor
+@Data
+@SequenceGenerator(name="seq_categoria_produto", sequenceName = "seq_categoria_produto", allocationSize = 1, initialValue = 1)
+public class CategoriaProduto implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_categoria_produto")
+	private Long id;
+	
+	@Column(nullable = false)
+	private String nomeDesc;
+	
+
+}
---
+++ src/main/java/com/loja_virtual/model/MarcaProduto.java
@@ -0,0 +1,31 @@
+package com.loja_virtual.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Entity
+@Table(name="marca_produto")
+@AllArgsConstructor
+@Data
+@SequenceGenerator(name="seq_marca_produto", sequenceName = "seq_marca_produto", allocationSize = 1, initialValue = 1)
+public class MarcaProduto implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_marca_produto")
+	private Long id;
+
+	@Column(nullable = false)
+	private String nomeDesc;
+}
