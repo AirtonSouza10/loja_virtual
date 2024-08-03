5c86104a69ebc271d37576fdc649e0279a4b1605
AirtonSouza10 <airtonsouza.ti@gmail.com>
2024-08-03 14:29:09 -0300

    Security

--- src/main/java/com/loja_virtual/controller/AcessoController.java
+++ src/main/java/com/loja_virtual/controller/AcessoController.java
@@ -1,21 +1,29 @@
 package com.loja_virtual.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.loja_virtual.model.Acesso;
 import com.loja_virtual.service.AcessoService;
 
 @Controller
+@RestController
 public class AcessoController {
 
 	@Autowired
 	private AcessoService acessoService;
 	
-	@PostMapping("/salvarAcesso")
-	public Acesso salvarAcesso(Acesso acesso) {
-		return acessoService.save(acesso);
+	@ResponseBody
+	@PostMapping(value = "**/salvarAcesso")
+	public ResponseEntity<Acesso> salvarAcesso(@RequestBody Acesso acesso) {
+		Acesso acessoSalvo = acessoService.save(acesso);
+		return new ResponseEntity<Acesso>(acessoSalvo, HttpStatus.OK);
 	}
 	
 }
